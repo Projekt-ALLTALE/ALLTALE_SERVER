0c8c8985947d71a31e0351891835a636d1e3bdcc
HoshinoSuzumi <boxlab@foxmail.com>
2021-08-28 05:59:24 +0800

    PM2 support

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    name: 'ALLTALE_SERVER',
+    script: 'app.js',
+    env: {
+        NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'hoshinosuzumi',
+      host : '42.193.21.101',
+      ref  : 'origin/main',
+      repo : 'git@github.com:Projekt-ALLTALE/ALLTALE_SERVER.git',
+      path : '~/Projects/ALLTALE_SERVER',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
