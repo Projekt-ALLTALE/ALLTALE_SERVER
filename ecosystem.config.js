ce2c179205fd3ced24db3885a7e94b3854f49c1e
HoshinoSuzumi <boxlab@foxmail.com>
2021-08-28 07:25:24 +0800

    sshkey

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
 
     deploy: {
         production: {
-            key: '~/.ssh/id_rsa.pub',
+            key: '~/.ssh/id_rsa',
             user: 'root',
             host: '42.193.21.101',
             ref: 'origin/main',
