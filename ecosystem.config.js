798cfdd5e7cade5a265ff4a77e2d2d79f5e17e65
HoshinoSuzumi <boxlab@foxmail.com>
2021-08-28 06:56:53 +0800

    update pm2 config 1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
 
     deploy: {
         production: {
-            key: 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMc2AUcP3OriV0pXW52sgghCG+uJckMbTz+PPcjTNrxnnsSFYSxkVkL3TDWsqAr6Qg5kR00LhBCGeidUDTTi2IRl6XDMyxl/4PC1JftV3JX0SLed6RRP+OnYFfVcwFXCTokTblDVhM3MN2lZ2zN/n88Z7Guv0jW6IRXAaaWnrEduiL7Vd99/F4DAAYq3dPv5kz1SAHxBpkzULJG5w7rNPhXp9JVHesDk+DHMkTMeYq6b4l3tOAshcsr3QbdT+RX5oyaVD52pTeNsAbuMacM++H6NDToJfg9Hrahz3sMivECPYJ103JVLWMGwoWosW+mqOuNIGQUpTaw0d4gpfgogdi/0Cs6UzdmuSOpllVvWO4nTE68KXqfIEEr9r2HM7PB5IUEQCTop7jSjUy/UAlCy+Uac3bwZyir097GexiSTrfgVOXP4K/KQn0z/LP1Bpt1kE6HGsEIl+hgUaOaGzuTEsUrNi8PBc9SuG/X79g+7BL9u3IlEf2Z31otmPyBZBYSac= root@0x0-server',
+            key: 'F:/sshkeys/0x0.pub',
             user: 'root',
             host: '42.193.21.101',
             ref: 'origin/main',
